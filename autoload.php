9c616274f266cfe5af178a893c6995873fc9ebb5
Sandor Fekete <info@sandorfekete.com>
2018-08-05 22:38:06 -0600

    Removed helper path in autoload as there is no helper directory anymore.

--- autoload.php
+++ autoload.php
@@ -8,16 +8,11 @@ if (!defined('PATH'))
 spl_autoload_register(function ($class_name){
     
     $pathCore = PATH . "/core/$class_name.php";
-    $pathHelper = PATH . "/helper/$class_name.php";
     $pathModel = PATH . "/model/$class_name.php";
 
     if (file_exists($pathCore))
     {
         require_once $pathCore;
-    }
-    else if (file_exists($pathHelper))
-    {
-        require_once $pathHelper;
     }
     else if (file_exists($pathModel))
     {
